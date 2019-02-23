8d27e417e76b1dba55b3fcb6fa8c8fc221ea6645
Fernando J da Silva <jsfernando@MacBook-Pro-de-Fernando.local>
2019-02-23 16:45:43 -0300

    inclusado firebase-config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,5 @@ export const environment = {
   production: true,
   firebase: firebaseconfig
   // {
-  //   apiKey: "AIzaSyAuzyevKEGZfBo7sfHVkKVVxp5DqwGy4as",
-  //   authDomain: "senac-app-campinas.firebaseapp.com",
-  //   databaseURL: "https://senac-app-campinas.firebaseio.com",
-  //   projectId: "senac-app-campinas",
-  //   storageBucket: "",
-  //   messagingSenderId: "940230609320"
   // }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,12 +9,6 @@ export const environment = {
   production: false,
   firebase: firebaseconfig
   // {
-  //   apiKey: "AIzaSyAuzyevKEGZfBo7sfHVkKVVxp5DqwGy4as",
-  //   authDomain: "senac-app-campinas.firebaseapp.com",
-  //   databaseURL: "https://senac-app-campinas.firebaseio.com",
-  //   projectId: "senac-app-campinas",
-  //   storageBucket: "",
-  //   messagingSenderId: "940230609320"
   // }
 };
 
